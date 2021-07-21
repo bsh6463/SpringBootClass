13abe6d6bcaadf9c36b1f180973616f0cc6a4931
bsh6463 <bsh9510@gmail.com>
2021-07-21 23:37:58 +0900

    210721, springboot class#2

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -1,8 +1,11 @@
 package hello.hellospring.controller;
 
+import lombok.Data;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -14,5 +17,32 @@ public class HelloController {
         //addAttribute(addtibuteName, attributeValue)
 
         return "hello";
+
+    }
+
+    @GetMapping("/hello-mvc")
+    public String helloMvc(@RequestParam String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("/hello-string")
+    @ResponseBody //http의 body에 data를 직접 넣어주겠다.
+    public String helloString(@RequestParam String name){
+        return "hello" + name;
+    }
+
+    @GetMapping("/hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    @Data
+    static class Hello{
+        private String name;
+
     }
 }
